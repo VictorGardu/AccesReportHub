6fb1f4aba90c5722a1a531e3932c324618ffce56
jlino11 <jesus.eduardo85@yahoo.com.mx>
2024-07-14 18:17:32 -0600

    Add dockerfile and Docker-compose with db config connection

--- src/app.js
+++ src/app.js
@@ -56,11 +56,11 @@ app.use(bodyParser.json());
 
 
 app.use(myconnection(mysql, {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    port: 3308,
-    database: 'arh'
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    port: process.env.DB_PORT || 3306,
+    database: process.env.DB_NAME || 'arh'
 }));
 
 
